fix calculation charbonnier loss

flyingChairsLoader.py
@@ -1,5 +1,4 @@
 import os, sys
-# from random import shuffle
 import numpy as np
 import cv2
 import subprocess
@@ -8,8 +7,10 @@ import utils as utils
 class flyingChairsLoader:
     '''Pipeline for preparing the Flying Chairs data
 
-    Image size: 1024 x 436
-    Training image pairs: 22872
+    Image size: 512 x 384
+    All image pairs: 22872
+    Train: 22232
+    Test: 640
 
     '''
 
@@ -17,12 +18,15 @@ class flyingChairsLoader:
         self.data_path = data_path
         self.image_size = image_size
         self.img_path = os.path.join(self.data_path, 'data')
-        # self.split = split
         # Read in the standard train/val split from http://lmb.informatik.uni-freiburg.de/resources/datasets/FlyingChairs.en.html
         self.trainValGT = self.trainValSplit()
         self.trainList, self.valList = self.getData(self.img_path)
         print("We have %d training samples and %d validation samples." % (len(self.trainList), len(self.valList)))
-
+        if False:    # Calculate the global training data mean
+            meanFile = self.calculateMean()
+            print("B: %4.4f G: %4.4f R: %4.4f " % (meanFile[0], meanFile[1], meanFile[2]))
+            # [97.533268117955444, 99.238235788550085, 97.055973199626948]
+            
     def trainValSplit(self):
         splitFile = "FlyingChairs_train_val.txt"
         if not os.path.exists(splitFile):
@@ -37,11 +41,11 @@ class flyingChairsLoader:
     def getData(self, img_path):
         assert(os.path.exists(img_path))
         numSamples = len(self.trainValGT)
-        print numSamples
+        print("There are %d image pairs in the dataset. " % numSamples)
         train = []
         val = []
         for imgIdx in xrange(numSamples):
-            frameID = "%05d" % (imgIdx + 1)
+            frameID = "%05d" % (imgIdx + 1)     # The image index starts at 00001
             if self.trainValGT[imgIdx][0] == "1":
                 train.append(frameID)
             elif self.trainValGT[imgIdx][0] == "2":
@@ -52,7 +56,6 @@ class flyingChairsLoader:
 
     def sampleTrain(self, batch_size, batch_id):
         assert batch_size > 0, 'we need a batch size larger than 0'
-        # batchSampleIdxs = np.random.choice(len(self.trainList), batch_size)
         batchSampleIdxs = xrange((batch_id-1)*batch_size, batch_id*batch_size)
         return self.hookTrainData(batchSampleIdxs)
 
@@ -68,17 +71,22 @@ class flyingChairsLoader:
             # print source.shape
             flow = utils.readFlow(os.path.join(self.img_path, (frameID + "_flow.flo")))
             # print flow.shape
-            source_list.append(np.expand_dims(cv2.resize(source, (self.image_size[1], self.image_size[0])), 0))
-            target_list.append(np.expand_dims(cv2.resize(target, (self.image_size[1], self.image_size[0])) ,0))
-            # flow_gt.append(np.expand_dims(cv2.resize(flow, (self.image_size[1], self.image_size[0])), 0))
+
+            source = cv2.resize(source, (self.image_size[1], self.image_size[0]))
+            source_list.append(np.expand_dims(source, 0))
+            target = cv2.resize(target, (self.image_size[1], self.image_size[0]))
+            target_list.append(np.expand_dims(target ,0))
+            
+            # # Data augmentation
+            # # Random scaling
+            # scale_factor = random.uniform(0.9, 1.1)
+            # source_scale =
+            
             flow_gt.append(np.expand_dims(flow, 0))
         return np.concatenate(source_list, axis=0), np.concatenate(target_list, axis=0), np.concatenate(flow_gt, axis=0)
-        # Adding the channel dimension if images are read in grayscale
-        # return np.expand_dims(source_list, axis = 3), np.expand_dims(target_list, axis = 3)  
 
     def sampleVal(self, batch_size, batch_id):
         assert batch_size > 0, 'we need a batch size larger than 0'
-        # batchSampleIdxs = np.random.choice(len(self.valList), batch_size)
         batchSampleIdxs = range((batch_id-1)*batch_size, batch_id*batch_size)
         return (self.hookValData(batchSampleIdxs), batchSampleIdxs)
 
@@ -94,10 +102,30 @@ class flyingChairsLoader:
             flow = utils.readFlow(os.path.join(self.img_path, (frameID + "_flow.flo")))
             source_list.append(np.expand_dims(cv2.resize(source, (self.image_size[1], self.image_size[0])), 0))
             target_list.append(np.expand_dims(cv2.resize(target, (self.image_size[1], self.image_size[0])) ,0))
-            # flow_gt.append(np.expand_dims(cv2.resize(flow, (self.image_size[1], self.image_size[0])), 0))
-            # flow_gt.append(np.expand_dims(flow, 0))e(flow, (self.image_size[1], self.image_size[0])), 0))
             flow_gt.append(np.expand_dims(flow, 0))
-        return np.concatenate(source_list, axis=0), np.concatenate(target_list, axis=0), np.concatenate(flow_gt, axis=0)       
+        return np.concatenate(source_list, axis=0), np.concatenate(target_list, axis=0), np.concatenate(flow_gt, axis=0)     
+
+    def calculateMean(self):
+        numSamples = len(self.trainList)
+        # OpenCV loads image as BGR order
+        B, G, R = 0, 0, 0
+        for idx in xrange(numSamples):
+            frameID = self.trainList[idx]
+            prev_img = frameID + "_img1.ppm"
+            next_img = frameID + "_img2.ppm"
+            source = cv2.imread(os.path.join(self.img_path, prev_img), cv2.IMREAD_COLOR)
+            target = cv2.imread(os.path.join(self.img_path, next_img), cv2.IMREAD_COLOR)
+            B += np.mean(source[:,:,0], axis=None)
+            B += np.mean(target[:,:,0], axis=None)
+            G += np.mean(source[:,:,1], axis=None)
+            G += np.mean(target[:,:,1], axis=None)
+            R += np.mean(source[:,:,2], axis=None)
+            R += np.mean(target[:,:,2], axis=None)
+        B = B / (2*numSamples)
+        G = G / (2*numSamples)
+        R = R / (2*numSamples)
+        return (B,G,R)
+
 
             
 